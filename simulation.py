d84a4731f15542d205096b731eb38e93c376d5fb
Martin Holecek <martin.holecek@deutsche-boerse.com>
2018-01-22 17:02:10 +0100

    Adding Damage, Speed and Distance

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import re
 import os
 import json
 import random
+import math
 
 ########
 # Globals
@@ -13,11 +14,13 @@ OUTFILE = 'simuResult'
 
 DEF_SETTING = {
     'glob_tries': 10,
-    'enemy_lives': 100,
+    'enemy_lives': 60,
     'enemy_wpn_missile': 4,
-    'enemy_wpn_beam': 6,
-    'enemy_success_misile': 6,
-    'enemy_success_beam': 5,
+    'enemy_wpn_beam': 8,
+    'enemy_success_missile': 6,
+    'enemy_success_beam': 4,
+    'enemy_speed': 20,
+    'ship_speed': 21,
     'ship_other_head': 40,
     'ship_other_body': 50,
     'ship_munition_storage': 4,
@@ -28,11 +31,21 @@ DEF_SETTING = {
     'ship_wpn_side_missile': 7,
     'ship_wpn_side_beam': 4,
     'ship_special_rooms': 4,
+    'ship_success_missile': 5,
+    'ship_success_beam': 4,
+    'ship_wpn_effectiveness': [2.2, 0.1],
 	'ship_max_damage': 20,
-    'init_distance': 10,
-    'glob_distance_close': 4,
+    'init_distance': 100,
+    'glob_distance_close': 40,
+    'glob_distance_death': 0,
     'glob_log_out': 10,
-    'sequence': [0,1,1],
+#    'sequence': [0,1,1],
+    'sequence': [2],
+    'enemy_lost_beam': 30,
+    'enemy_lost_missile': 20,
+    'enemy_lost_speed': 15,
+    'repair_crews': 10,
+    'repair_delay': 5,
 }
 
 STANDS = [
@@ -40,21 +53,29 @@ STANDS = [
         'name': 'Run',
         'enemy_success': -2,
         'poll': 'poll_head',
+        'weapons': 'head',
+        'speed': 0,
     },
     {
         'name': 'Dodge',
         'enemy_success': -1,
         'poll': 'poll_head',
+        'weapons': 'head',
+        'speed': -3,
     },
     {
         'name': 'Side',
         'enemy_success': 0,
         'poll': 'poll_side',
+        'weapons': 'side',
+        'speed': -6,
     },
     {
         'name': 'Wedge',
         'enemy_success': +2,
         'poll': 'poll_side',
+        'weapons': 'none',
+        'speed': -6,
     },
 ]
 
@@ -186,6 +207,50 @@ def getCICReport( damage, ship ):
 		report.append([d, ship[d]])
 	return report
 
+def getShipWeapons( stand, setting, state, turnCount ):
+    out = [0, 6]
+    wpnType = 'missile' if state['onDistance'] else 'beam'
+    wpnStand = stand['weapons']
+    if wpnStand == 'none': return out
+    out[1] = setting['ship_success_'+wpnType]
+
+    count = setting['ship_wpn_'+wpnStand+'_'+wpnType] - state[wpnStand+'_'+wpnType+'_down']
+    effect = setting['ship_wpn_effectiveness'][0] - (setting['ship_wpn_effectiveness'][1] * state['wpn_effectiveness_down'])
+
+    count1 = count * effect
+    count2 = math.floor(count1)
+    if (count1 != count2) and (turnCount % 2 == 0): count2 += 1
+    out[0] = int(count2)
+    return out
+
+def getEnemyWeapons( state, setting ):
+    wpn = 'missile' if state['onDistance'] else 'beam'
+    return [
+        int(max(setting['enemy_wpn_'+wpn]-(math.floor(state['enemy_damage'] / setting['enemy_lost_beam'])), 1)),
+        setting['enemy_success_'+wpn]+stand['enemy_success']
+    ]
+
+def addDamagedSystem( state, hit ):
+    if hit[0] == 'V': # Missile
+        if hit[1] == '0':
+            state['head_missile_down'] += 1
+        else:
+            state['side_missile_down'] += 1
+    elif hit[0] == 'D': # Beam
+        if hit[1] == '0':
+            state['head_beam_down'] += 1
+        else:
+            state['side_beam_down'] += 1
+    elif hit[0] == 'M' or hit[0] == 'S': # Service
+        state['wpn_effectiveness_down'] += 1
+    elif hit[0] == 'P': # Rings
+        state['speed_down'] += 1
+
+def calcNewSpeed( setting, state ):
+    out = setting['enemy_speed']-setting['ship_speed']-STANDS[state['stand']]['speed']
+    out += state['speed_down'] - math.floor(state['enemy_damage']/setting['enemy_lost_speed'])
+    return int(out)
+
 ########
 # Process
 
@@ -236,10 +301,10 @@ poll_side = ['R0','R1','K0','K1'];
 poll_side.extend(addShipParts('X', setting['ship_special_rooms']))
 poll_side.extend(addShipParts('M', setting['ship_munition_storage']))
 poll_side.extend(addShipParts('S', setting['ship_service_room']))
-poll_side.extend(addShipParts('P', setting['ship_ring']))
+poll_side.extend(addShipParts('P0', setting['ship_ring']))
 poll_side.extend(addShipParts('P5', setting['ship_ring']))
-poll_side.extend(addShipParts('V', setting['ship_wpn_head_missile']))
-poll_side.extend(addShipParts('D', setting['ship_wpn_head_beam']))
+poll_side.extend(addShipParts('V0', setting['ship_wpn_head_missile']))
+poll_side.extend(addShipParts('D0', setting['ship_wpn_head_beam']))
 poll_side.extend(addShipParts('V5', setting['ship_wpn_side_missile']))
 poll_side.extend(addShipParts('D5', setting['ship_wpn_side_beam']))
 
@@ -251,9 +316,9 @@ poll_side.extend(addShipParts('O', other_head))
 poll_side.extend(addShipParts('O5', other_body))
 
 poll_head = ['R0', 'S0', 'M0', 'X0']
-poll_head.extend(addShipParts('P', setting['ship_ring']))
-poll_head.extend(addShipParts('V', setting['ship_wpn_head_missile']))
-poll_head.extend(addShipParts('D', setting['ship_wpn_head_beam']))
+poll_head.extend(addShipParts('P0', setting['ship_ring']))
+poll_head.extend(addShipParts('V0', setting['ship_wpn_head_missile']))
+poll_head.extend(addShipParts('D0', setting['ship_wpn_head_beam']))
 poll_head.extend(addShipParts('O', other_head))
 
 # Run the Simulation
@@ -261,73 +326,105 @@ poll_head.extend(addShipParts('O', other_head))
 results = {}
 for tryIndex in range(0, setting['glob_tries']):
 	# One Game
-	state = {
-		'distance': setting['init_distance'],
-		'onDistance': True,
-		'stand': setting['sequence'][0],
-		'sequenceLength': len(setting['sequence']),
-		'damage': 0,
-		'victory': False,
-		'lost': False,
-	}
-	
-	poll = {
-		'poll_head': poll_head[:],
-		'poll_side': poll_side[:],
-	}
-	toRepair = []
-	destroyed = []
-	
-	ship = {};
-	for key in poll_side:
-		ship[key] = 0
-	
-	turnCount = 0;
-		
-	addLog(0, '==> Game #'+str(tryIndex+1)+' ==')
-	while True:
-		turnCount += 1
-		# One Turn
-		distText = 'on Distance' if state['onDistance'] else 'up Close'
-		stand = STANDS[state['stand']]
-		addLog(0, 'Turn #'+str(turnCount)+', '+distText+', '+stand['name'])
+    state = {
+        'distance': setting['init_distance'],
+        'onDistance': True,
+        'stand': setting['sequence'][0],
+        'sequenceLength': len(setting['sequence']),
+        'damage': 0,
+        'victory': False,
+        'lost': False,
+        'head_missile_down': 0,
+        'head_beam_down': 0,
+        'side_missile_down': 0,
+        'side_beam_down': 0,
+        'wpn_effectiveness_down': 0,
+        'speed_down': 0,
+        'enemy_damage': 0,
+        'our_damage_total': 0,
+        'speed': setting['enemy_speed']-setting['ship_speed']-STANDS[setting['sequence'][0]]['speed'],
+    }
 
+    poll = {
+        'poll_head': poll_head[:],
+        'poll_side': poll_side[:],
+    }
+    toRepair = []
+    destroyed = []
+
+    ship = {};
+    for key in poll_side:
+        ship[key] = 0
+
+    turnCount = 0;
+
+    addLog(0, '==> Game #'+str(tryIndex+1)+' ==')
+    while True:
+        turnCount += 1
+		# One Turn
+        distText = 'on Distance' if state['onDistance'] else '!! UP CLOSE !!'
+        stand = STANDS[state['stand']]
+        we = getShipWeapons(stand, setting, state, turnCount)
+        enemy = getEnemyWeapons(state, setting)
+        addLog(0, 'Turn #'+str(turnCount)+', Distance: '+str(state['distance'])+' '+distText+', Stand: '+stand['name']+', Closing Speed: '+str(state['speed']))
+        addLog(2, ' - We: '+str(we[0])+'('+str(we[1])+'), Enemy: '+str(enemy[0])+'('+str(enemy[1])+')')
 		# Enemy Fires
-		enemy = [setting['enemy_wpn_missile'], setting['enemy_success_misile']+stand['enemy_success']]
-		if not state['onDistance']: enemy = [setting['enemy_wpn_beam'], setting['enemy_success_beam']+stand['enemy_success']]
-		hits = rollDice(enemy[0], enemy[1])		
-		pollHits = doPoll(poll[stand['poll']], hits)
-		addLog(4, ' - Enemy hits: '+str(pollHits))
+        hits = rollDice(enemy[0], enemy[1])
+        pollHits = doPoll(poll[stand['poll']], hits)
+        state['our_damage_total'] += hits
+        addLog(4, ' - Enemy hits: '+str(pollHits)+ ' ('+str(state['our_damage_total'])+')')
 
 		# Do Damage
-		for hit in pollHits:
-			if ship[hit] == 2:
-				state['damage'] += doDamage(hit, True)
-				ship[hit] = 3
-				poll['poll_head'].remove(hit)
-				poll['poll_side'].remove(hit)
-				destroyed.append(hit)
-				toRepair.remove(hit)
-			elif ship[hit] == 1:
-				state['damage'] += doDamage(hit, False)
-				ship[hit] = 2
-			elif ship[hit] == 0:
-				ship[hit] = 1
-				toRepair.append(hit)
-		
+        for hit in pollHits:
+            if ship[hit] == 2:
+                state['damage'] += doDamage(hit, True)
+                ship[hit] = 3
+                if hit in poll['poll_head']: poll['poll_head'].remove(hit)
+                poll['poll_side'].remove(hit)
+                destroyed.append(hit)
+                toRepair.remove(hit)
+            elif ship[hit] == 1:
+                state['damage'] += doDamage(hit, False)
+                ship[hit] = 2
+            elif ship[hit] == 0:
+                ship[hit] = 1
+                toRepair.append(hit)
+                addDamagedSystem(state, hit)
+
+        # We Fires
+        hits = rollDice(we[0], we[1])
+        state['enemy_damage'] += hits
+        addLog(4, ' - We hits: +'+str(hits)+' ('+str(state['enemy_damage'])+'/'+str(setting['enemy_lives'])+')')
+
+
 		# Lost Condition
-		if state['damage'] >= setting['ship_max_damage']:
-			state['lost'] = True
-			break
-		
+        if state['damage'] >= setting['ship_max_damage']:
+            state['lost'] = True
+            break
+
+		# Win Condition
+        if state['enemy_damage'] >= setting['enemy_lives']:
+            state['victory'] = True
+            break
+
 		# Ship Summary
-		addLog(2, ' - Ship Damage('+str(state['damage'])+'/'+str(setting['ship_max_damage'])+'): '+str(getCICReport(toRepair, ship)))
-		addLog(2, ' - - Destroyed: '+str(destroyed))	
+        addLog(2, ' - Ship Damage('+str(state['damage'])+'/'+str(setting['ship_max_damage'])+'): '+str(getCICReport(toRepair, ship)))
+        addLog(2, ' - - Destroyed: '+str(destroyed))
+
+		# Calc new Distances
+        state['distance'] -= state['speed']
+        state['onDistance'] = state['distance'] > setting['glob_distance_close']
+
+		# Lost Distance Condition
+        if state['distance'] <= setting['glob_distance_death']:
+            state['lost'] = True
+            break
 
 		# Evasive Action
-		state['stand'] = setting['sequence'][turnCount % state['sequenceLength']]
+        state['stand'] = setting['sequence'][turnCount % state['sequenceLength']]
+        state['speed'] = calcNewSpeed(setting, state)
 
-	addLog(0, '')
-	if state['lost']: addLog(0, 'GAME LOST')
-	if state['victory']: addLog(0, 'VICTORY')
-	addLog(0, '')
+    addLog(0, '')
+    if state['lost']: addLog(0, 'GAME LOST')
+    if state['victory']: addLog(0, 'VICTORY')
+    addLog(0, '')
